b364f4d0a29fd7b546b8ab752e86853d0849def8
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-03-19 14:58:51 +0100

    Created project + enums

---
+++ FairPoker/FairPoker/Enums/CardColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPoker.Enums
+{
+    enum CardColor
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clovers,
+    }
+}
