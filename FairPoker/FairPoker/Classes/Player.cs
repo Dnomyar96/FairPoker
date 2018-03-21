9651198f28d33d197ad910ca09b06027c7bdf41b
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-03-21 14:03:08 +0100

    Added dealer + player

---
+++ FairPoker/FairPoker/Classes/Player.cs
@@ -0,0 +1,63 @@
+﻿using FairPoker.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPoker.Classes
+{
+    public class Player
+    {
+        private List<Card> hand;
+        private int cash;
+        private Score score;
+
+        public Player()
+        {
+            hand = new List<Card>();
+            cash = 1000;
+            score = Score.HighCard;
+        }
+
+        public void GiveCard(Card card)
+        {
+            hand.Add(card);
+        }
+
+        public IEnumerable<Card> GetCards()
+        {
+            return hand;
+        }
+
+        public void CalculateScore(List<Card> cards)
+        {
+            score = Scoring.GetScore(hand.Concat(cards));
+        }
+
+        public Score GetScore()
+        {
+            return score;
+        }
+
+        public void Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Bet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Raise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fold()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
