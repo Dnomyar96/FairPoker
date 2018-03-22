aaa3471e678d48edda955e20d549ff0337ee7584
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-03-22 14:34:53 +0100

    Started Chancecalculation

--- FairPoker/FairPoker/Classes/ChanceCalculator.cs
+++ FairPoker/FairPoker/Classes/ChanceCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FairPoker.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +7,59 @@ using System.Threading.Tasks;
 
 namespace FairPoker.Classes
 {
-    class ChanceCalculator
+    public static class ChanceCalculator
     {
+        private static List<Card> cardsInDeck;
+        private static int cardsPerColor;
 
-        int players;
-        int deckRemaining;
-        Card tablecardA;
-        Card tablecardB;
-        Card tablecardC;
-        Card tablecardD;
-        Card tablecardE;
+        static ChanceCalculator()
+        {
+            cardsPerColor = Enum.GetValues(typeof(CardColor)).Length;
 
-        
+            var cards = new List<Card>();
 
+            foreach (CardColor color in Enum.GetValues(typeof(CardColor)))
+            {
+                foreach (CardValue value in Enum.GetValues(typeof(CardValue)))
+                {
+                    cards.Add(new Card
+                    {
+                        Color = color,
+                        Value = value,
+                        ImgUrl = "ms-appx:///Assets/Cards/" + (value.ToString() + color.ToString()) + ".png"
+                    });
+                }
+            }
 
+            cardsInDeck = cards;
+        }
 
+        public static double PairChance(IEnumerable<Card> cards)
+        {
+            if (!Scoring.IsPair(cards))
+            {
+                var knownCardCount = Convert.ToDouble(cards.Count());
+                var unknownCardCount = Convert.ToDouble(cardsInDeck.Count() - knownCardCount);
 
+                var chancePerCard = 3 / unknownCardCount * 100;
+                return chancePerCard * knownCardCount;
+            }
+            else
+            {
+                return 100;
+            }
+        }
 
+        //public static double FlushChance(IEnumerable<Card> cards)
+        //{
+        //    if (!Scoring.IsFlush(cards))
+        //    {
+                
+        //    }
+        //    else
+        //    {
+        //        return 100;
+        //    }
+        //}
     }
 }
