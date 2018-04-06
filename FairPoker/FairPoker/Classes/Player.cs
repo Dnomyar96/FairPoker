7bca626887bfefe75fc2ea66602a5e50bce6f883
Daan Broere <broeredaan@users.noreply.github.com>
2018-04-06 13:27:37 +0200

    ugh kut zooi

--- FairPoker/FairPoker/Classes/Player.cs
+++ FairPoker/FairPoker/Classes/Player.cs
@@ -171,17 +171,16 @@ namespace FairPoker.Classes
         {
             state = PlayerState.Folded;
         }
-
-<<<<<<< HEAD
+        
         public int GetPlayerBet()
         {
             return bet;
-=======
+        }
+
         public async Task Turn()
         {
             AIDecisionHandler aIDecisionHandler = new AIDecisionHandler(this);
             Debug.WriteLine(AIDecisionHandler.MakeDecision());
->>>>>>> 83eb79a50cd40adc66815553d39e83e93a55ae3f
         }
     }
 }
